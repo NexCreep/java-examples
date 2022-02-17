145e378e7f6dca66857385dbd9fd6a555e5fae15
raulop194 <raulop194@alu.centroafuera.es>
2022-02-17 13:37:00 +0100

    Update 17/02 13:36

---
+++ Basico/HolaMundo.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class HolaMundo here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class HolaMundo
+{
+    public static void main(String[] args){
+        System.out.println("Hola mundo!");
+    }
+}
