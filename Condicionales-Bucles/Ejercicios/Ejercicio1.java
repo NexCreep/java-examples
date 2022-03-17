8508060c00e70ed2d8d9005ac0536b7bb82c33f6
raulop194 <raulop194@alu.centroafuera.es>
2022-03-17 10:44:42 +0100

    Added package.bluej to the repository

--- Condicionales-Bucles/Ejercicios/Ejercicio1.java
+++ Condicionales-Bucles/Ejercicios/Ejercicio1.java
@@ -3,6 +3,8 @@ package Ejercicios;
  * Año: _2022_
  * Mes: _03_
  * Día: _16_
+ * 
+ * Han pasado 75 dias desde comienzo de año
  */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -19,67 +21,54 @@ public class Ejercicio1
         System.out.print("Día: ");
         int dia = Integer.parseInt(buffer.readLine());
 
-        if (mes <= 12 && mes > 0){
+        if ((mes <= 12 && mes > 0) && (dia > 0 && dia <= 31)){
             int febr = 28;
-            int mDias = 0;
-            int dias = 0;
 
             if (año % 4 == 0)
                 febr = 29;
 
-            switch (mes){
-                case 12:
-                    if (mes == 12)
-                        mDias = 31;
-                    dias = dias + 31;
-                case 11:
-                    if (mes == 11)
-                        mDias = 30;
-                    dias = dias + 30;
-                case 10: 
-                    if (mes == 10)
-                        mDias = 31;
-                    dias = dias + 31;
-                case 9:
-                    if (mes == 9)
-                        mDias = 30;
-                    dias = dias + 30;
-                case 8:
-                    if (mes == 8)
-                        mDias = 31;
-                    dias = dias + 31;
-                case 7:
-                    if (mes == 7)
-                        mDias = 31;
-                    dias = dias + 31;
-                case 6:
-                    mDias = 30;
-                    dias = dias + 30;
-                case 5:
-                    mDias = 31;
-                    dias = dias + 31;
-                case 4:
-                    mDias = 30;
-                    dias = dias + 30;
-                case 3:
-                    mDias = 31;
-                    dias = dias + 31;
-                case 2:
-                    if ()
-                    mDias = 31;
-                    dias = dias + 31 + febr;
-                    break;
+            int dias = 0;
+            if (mes > 2){
+                int mPar = 0;
+                int mImp = 0;
+                
+                switch (mes-1){
+                    case 11:
+                        mPar+=1;
+                    case 10: 
+                        mImp+=1;
+                    case 9:
+                        mPar+=1;
+                    case 8:
+                        mImp+=1;
+                    case 7:
+                        mImp+=1;
+                    case 6:
+                        mPar+=1;
+                    case 5:
+                        mImp+=1;
+                    case 4:
+                        mPar+=1;
+                    case 3:
+                        mImp+=1;
+                    case 2:
+                        dias+=febr;
+                        mImp+=1;
+                        break;
+                }
+                dias = (dias + (30 * mPar) + (31 * mImp)) + dia;
             }
-            
-            System.out.println("Dias año: " + dias);
-
-            if (dias > 0)
-                System.out.println(dias - (mDias - dia));
-            else
-                System.out.println(dia);
+            else{
+                dias = dia;
+                if (mes > 1)
+                    dias += 31;
+            }
+            System.out.printf("Han pasado %d dias desde comienzo de año hasta el %d/%d/%d. \n", dias, dia, mes, año);
+        }                
+        else{
+            System.out.println("El mes no puede ser mayor a 12 o menor que 1");
+            System.out.println("Ni el dia no puede ser mayor a 31 o menor que 1");
         }
-        else
-            System.out.println("El mes no puede ser mayor a 12 y menor que 1");
 
     }
 }
