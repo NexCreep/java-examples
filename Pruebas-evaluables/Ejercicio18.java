048cb23b9cc380d6653c1a5b1463e044c08ac709
NexCreep <codenexcreep@gmail.com>
2022-04-04 21:48:16 +0200

    Update 04/04 21:48

--- Pruebas-evaluables/Ejercicio18.java
+++ Pruebas-evaluables/Ejercicio18.java
@@ -1,15 +1,41 @@
 import java.util.Arrays;
+
 public class Ejercicio18
 {
-   public static void main(int capital, int[] tiposInteres, int plazo){
-       int[] capInt = new int[tiposInteres.length];
+   public static void main(/*float capital, int[] tiposInteres, int plazo*/){
+       double capital = 10000f;
+       byte[] intereses = new byte[]{10, 11, 12, 13, 14, 15};
+       byte plazo = (byte)15;
        
-       for (int i=0; i<capInt.length; i++){
-           capInt[i] = capital;
+       /*String capitalStr = String.valueOf(capital);
+       int capitalLen = capitalStr.length() + 2;
+       String spaces
+       System.out.println(capitalLen);*/
+       
+       
+       for (byte interes : intereses)
+           System.out.printf("      %d%%   ", interes);
+        System.out.println();
+       
+       double[] crecimiento = new double[intereses.length];
+       for(int i=0; i<crecimiento.length; i++){
+           crecimiento[i] = capital;
+           System.out.printf("%,.2f   ", crecimiento[i]);
+       }
+       System.out.println();
+    
+       for(int año=1; año<=plazo; año++){
+           for(int i=0; i<crecimiento.length; i++){
+                double incremento = crecimiento[i] * (intereses[i] / 100f);
+                crecimiento[i] += incremento;
+                
+                System.out.printf("%,.2f   ", crecimiento[i]);
+            }
+            if (año == plazo) System.out.printf("--Monto final");
+            System.out.println();
        }
        
-       System.out.println(Arrays.toString(tiposInteres));
-       System.out.println(Arrays.toString(capInt));
-       
+        
+    
    }
 }
