c8e6cb9f39363da78b5e99a3dbd9c7ae82e0d285
raulop194 <raulop194@alu.centroafuera.es>
2022-04-27 14:19:23 +0200

    Update 27/04 14:19

---
+++ Programacion-Orientada-A-Objetos/Extra/Abstraccion.java
@@ -0,0 +1,19 @@
+/*
+ * La abstraccion de una clase permite que cuando una clase la herede
+ * deba de redefinir sus metodos abstractos. 
+ * 
+ * Las clases abstractas no se pueden inicializar
+ * 
+ * Los metodos abstractos:
+ *      - Al menos un método abstracto
+ *      - Métodos abtractos unicamente en clase abstracta
+ *      - Los métodos abstractos no tienen cuerpo
+ *      - Pueden contener método no abstracto
+ * 
+*/
+public abstract class Abstraccion
+{
+    public abstract void print(String msg);
+    
+    
+}
