799a79c2330e4675d443a95c461733808ab6385c
raulop194 <raulop194@alu.centroafuera.es>
2022-05-09 14:09:06 +0200

    Update 09/05 14:09

--- Control-Excepciones/Excepciones.java
+++ Control-Excepciones/Excepciones.java
@@ -1,29 +1,35 @@
 import java.util.Scanner;
 
 public class Excepciones
-{
-    public static void main(){
+{   
+    public static void main(String[] args){
+        int num1 = getNum("Numero 1: ");
+        int num2 = getNum("Numero 2: ");
+        int num3 = getNum("Numero 3: ");
         
-        askNum();
-        
-        System.out.println("Fin de proceso");
+        System.out.print("Suma: " + (num1 + num2 + num3));
     }
     
-    public static void askNum() {
-        final Scanner scan = new Scanner(System.in);
+    private static int getNum(String msg){
+        Scanner sc = new Scanner(System.in);
         
-        try{
-            System.out.print("Numero: ");
-            int a = Integer.parseInt(scan.nextLine());
-            System.out.println("El numero es: " + a);
-        }
-        catch (NumberFormatException e){
-            System.out.println("El numero introducido no tiene el formato adecuado o no es un numero");
-            askNum();
-        }
-        catch (Exception e){
-            System.out.println(e);
-            askNum();
-        }
+        boolean error = true;
+        int num = 0;
+        
+        error = true;
+        do{
+            try{
+                System.out.print(msg);
+                num = Integer.parseInt(sc.nextLine());
+                error = false;
+            }catch (NumberFormatException e){
+                System.out.println("Debes de introducir un numero");
+            }catch (Exception e){
+                System.out.println(e);
+            }
+        }while (error);
+        
+        return num;
     }
+    
 }
